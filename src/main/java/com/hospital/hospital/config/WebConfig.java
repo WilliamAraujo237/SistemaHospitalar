1016cc7d222af23b87c6bef572067c7a5884f9a4
WilliamAraujo237 <william237.araujo@gmail.com>
2023-12-21 17:26:22 -0300

    Adicionando configuração na pasta config para serelizar json e xml via header

--- src/main/java/com/hospital/hospital/config/WebConfig.java
+++ src/main/java/com/hospital/hospital/config/WebConfig.java
@@ -8,8 +8,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+/*
+        //Via Query param Exemplo { http://localhost:8080/Cargo/ListaCargos?MediaType=xml }
         configurer.favorParameter(true)
                 .parameterName("MediaType").ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("xml",MediaType.APPLICATION_XML)
+                .mediaType("json",MediaType.APPLICATION_JSON);
+
+ */
+
+
+        //Via Header param Exemplo { http://localhost:8080/Cargo/ListaCargos }
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("xml",MediaType.APPLICATION_XML)
